4f6cce1df5deacb872fa84e04b6fb3b273303c4c
simals89 <47834679+simals89@users.noreply.github.com>
2022-08-22 13:23:34 +0200

    Update profiling.py

--- profiling.py
+++ profiling.py
@@ -3,6 +3,8 @@ import scipy.sparse as sp
 import cProfile
 from project import SparseMatrix
 
+#### change() ####
+
 # Create matrix
 np.random.seed(10)
 A = np.zeros((10000,10000))
@@ -19,3 +21,25 @@ M_scipy = sp.csr_matrix(G)
 # Run cProfile
 cProfile.run("M_sparse.change(0,0,99)")
 cProfile.run("scipyChange(M_scipy, 0, 0, 99)")
+
+#### add() ####
+
+# Create random matrices
+np.random.seed(10)
+M = np.zeros((1000,1000))
+idx = np.random.choice(np.linspace(0,np.shape(M)[0]*np.shape(M)[0]-1,np.shape(M)[0]*np.shape(M)[0]), 
+                       size = int(np.shape(M)[0]*np.shape(M)[0]*0.1), replace = False).astype(int)
+M[np.unravel_index(idx, M.shape)] = np.random.randint(0, 100, int((np.shape(M)[0]*np.shape(M)[0]*0.1)))
+
+# Create SparseMatrix
+M1_sparse = SparseMatrix(M)
+M2_sparse = SparseMatrix(M)
+
+
+# Create CSR matrix using SciPy
+M1_scipy = sp.csr_matrix(M)
+M2_scipy = sp.csr_matrix(M)
+
+# Run cProfile
+cProfile.run("M1_sparse + M2_sparse")
+cProfile.run("M1_scipy + M2_scipy")
